12b4684fb5050712c87115863543c0ac5eca558a
Giovanni Ricca <giovanniricca@proton.me>
2022-08-23 12:53:46 +0400

    NB1: camera: Report the correct QCamera3Stream size
    * Required due to clang 13 changes
    
    Change-Id: Ic0cd224b3ce5ceaf02be845e5185e0309e440084

--- camera/QCamera2/HAL3/QCamera3Stream.cpp
+++ camera/QCamera2/HAL3/QCamera3Stream.cpp
@@ -55,7 +55,8 @@ const char* QCamera3Stream::mStreamNames[] = {
         "CAM_RAW",
         "CAM_OFFLINE_PROC",
         "CAM_PARM",
-        "CAM_ANALYSIS" };
+        "CAM_ANALYSIS",
+        "CAM_MAX" };
 
 /*===========================================================================
  * FUNCTION   : get_bufs
